7bacc357cff5710912486046675ab1c356c09e07
AnryChe <stroyspecialist@rambler.ru>
2020-03-16 15:34:18 +0200

    sj

--- Parser.py
+++ Parser.py
@@ -36,28 +36,35 @@ get_requests = {'SuperJob': 'vacancy/search/?keywords=', 'HeadHunter':'search/va
 #         print(hh_parced)
 #     print("Вакансия сайта: ", sites[s])
 
-vacansy = 'python'
+vacansy = 'java developer'
 # vacansy = input('Введите наименование вакансии: ')
 vacansys = []
 
 # ПОИСК ВАКАНСИЙ НА SUPERJOB
-# Готовим ссылку и "готовим суп"
-
 main_link_sj = sites['SuperJob']+get_requests['SuperJob']+vacansy
-# print(main_link_sj)
-response = requests.get(main_link_sj, headers = headers)
-parced_html = bs(response.text, 'lxml')
-# print(parced_html)
 
+# Готовим  суп
+def sj_soup(page):
+    soup_link_sj = main_link_sj+'&page='+str(page)
+    # print(main_link_sj)
+    response = requests.get(soup_link_sj, headers = headers)
+    parced_html = bs(response.text, 'lxml')
+    return parced_html
+    # print(parced_html)
+
+sj_soup(1)
 
 # Находим и указываем в переменных классы блока вакансии, блока названия вакансии, блока зарплаты
-sj_div_class = '_3zucV f-test-vacancy-item undefined RwN9e _3tNK- _1I1pc'
+sj_div_class = 'acdxh GPKTZ _1tH7S'
 sj_div_class_vac_name = '_3mfro CuJz5 PlM3e _2JVkc _3LJqf'
 sj_div_class_salary = '_3mfro _2Wp8I _31tpt f-test-text-company-item-salary PlM3e _2JVkc _2VHxz'
-pages=0
+
+#  Итоговый словарь
+vac_data = {}
 
 #  Функция поиска вакансий
-def find_data():
+def find_data(page):
+    parced_html = sj_soup(page)
     # Переменные наименования вакансии, зарплаты:
     sj_parced_name = parced_html.find('div', {'class': sj_div_class_vac_name}).getText()
     sj_parced_salary = parced_html.find('span', {'class': sj_div_class_salary})
@@ -70,60 +77,51 @@ def find_data():
             sj_curency = '₽'
         else:
             sj_parced_salary_txt = sj_parced_salary.getText()
-            #print(re.findall('до$', sj_parced_salary_txt))
             if 'По договоренности' in sj_parced_salary_txt:
-                # print('По договорённости')
                 sj_parced_salary_min = 0
                 sj_parced_salary_max = 0
                 sj_curency = '₽'
             elif 'от' in sj_parced_salary_txt:
-                # print('от')
                 sj_parced_salary_min = (re.findall('\d{3}.?\d{3}', sj_parced_salary_txt))[0].replace(u'\xa0', u'')
                 sj_parced_salary_max = 0
                 sj_curency = sj_parced_salary_txt[-1]
             elif 'до' in sj_parced_salary_txt:
-                print('до')
                 sj_parced_salary_max = sj_parced_salary_txt
                 sj_parced_salary_min = 0
                 sj_curency = sj_parced_salary_txt[-1]
             elif '—' in sj_parced_salary_txt:
-                # print('-')
                 sj_parced_salary_price = re.findall('\d{3}.?\d{3}', sj_parced_salary_txt)
                 sj_parced_salary_max = sj_parced_salary_price[1].replace(u'\xa0', u'')
                 sj_parced_salary_min = sj_parced_salary_price[0].replace(u'\xa0', u'')
                 sj_curency = sj_parced_salary_txt[-1]
             else:
                 print('Что то пошло не так...')
-            print('Сайт: ', 'Суперджоб', 'Вакансия: ', sj_parced_name, '. Зарплата от', sj_parced_salary_min, 'до', sj_parced_salary_max, sj_curency)
-        vac_data = {}
+        # print('Сайт: ', 'Суперджоб', 'Вакансия: ', sj_parced_name, '. Зарплата от', sj_parced_salary_min, 'до', sj_parced_salary_max, sj_curency)
+
         vac_data['Site'] = sites['SuperJob']
         vac_data['name'] = sj_parced_name
         vac_data['salary_min'] = sj_parced_salary_min
         vac_data['salary_max'] = sj_parced_salary_max
-        vac_data['salary_link'] = page_sj
-        vacansys.append(vac_data)
+        return vacansys.append(vac_data)
 
 # Считаем страницы
+
 sj_div_class_page = 'L1p51'
-sj_parced_page = parced_html.find('div', {'class': sj_div_class_page})
+sj_parced_page = sj_soup(1).find('div', {'class': sj_div_class_page})
 if sj_parced_page:
     pages = len(sj_parced_page.findChildren(recursive=False)) - 2
 else: pages=0
 
-
 # В зависимости от количества страниц обрабатываем вакансии. Если страница одна - просто запускаем функцию  выбора, если больше одной - пускаем в цикл и парсим остальные страницы
+find_data(1)
 if pages >0:
     #  Готовим перечень страниц
     pages_sj = []
     for i in range(pages):
         page_sj = main_link_sj+'&page='+str(i+1)
         pages_sj.append(page_sj)
-    find_data()
+    print(pages_sj[1:])
     for page in pages_sj[1:]:
-        response = requests.get(page, headers=headers)
-        parced_html = bs(response.text, 'lxml')
-        find_data()
-else:
-    find_data()
+        find_data(page)
 
 print(vacansys)
