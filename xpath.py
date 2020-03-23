e2a76ef50805a6210c21626e5573784b9c4cae51
AnryChe <stroyspecialist@rambler.ru>
2020-03-23 16:43:07 +0200

    Added parsing from korrespondent.net

--- xpath.py
+++ xpath.py
@@ -20,28 +20,25 @@ def get_html(main_links):
 
         div_class = main_links.get(link)
         response = requests.get(link, headers = headers).text
-        # print(response)
         tree = html.fromstring(response)
-        news_data = {}
         if link == 'https://korrespondent.net':
             source_name = 'Корреспондент'
-            news = tree.xpath("//div[@class='time-articles']//*")
-            for new in news:
-                news_name = news.xpath(".//a/text()")
-                print(news_name)
-                news_time = news.xpath("//div[@class='time-articles']//div[@class='article__time']//text()")
-                news_link = tree.xpath("//div[@class='time-articles']//a/@href")
+            news_items = tree.xpath("//div[@class='time-articles']/*")
+            print(len(news_items))
+            for i in news_items:
+                news_data = {}
+                news_name = i.xpath(".//a/text()")
+                news_time = i.xpath(".//div[@class='article__time']//text()")
+                news_link = i.xpath(".//a/@href")
+                news_data['source'] = source_name
+                news_data['name'] = news_name
+                news_data['time'] = news_time
+                news_data['link'] = news_link
+                news_list.append(news_data)
         elif link == 'https://www.rbc.ua':
             pass
-
-            # news_data['source'] = source_name
-            # news_data['time'] = news_times
-            # news_data['link'] = news_links[news_name]
-            # news_list.append(news_data)
+        print(news_list)
 
 
 get_html(main_links)
-print(news_list)
-def get_korr(tree):
-    # name = tree.xpath("/div[@class='time-articles']//div[@class='time-articles']/a/text()")
-    pass
+
