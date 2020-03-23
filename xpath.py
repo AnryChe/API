4a84522ac5808a78204240724053c0ab32aa2642
AnryChe <stroyspecialist@rambler.ru>
2020-03-23 19:36:21 +0200

    add mongo db

--- xpath.py
+++ xpath.py
@@ -11,6 +11,8 @@
 import requests
 from pprint import pprint
 from lxml import html
+from pymongo import MongoClient
+
 main_links = {r'https://korrespondent.net': 'time-articles', r'https://www.rbc.ua': 'content-section', r'https://www.ukr.net/ru': 'feed'}
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:74.0) Gecko/20100101 Firefox/74.0'}
 
@@ -20,10 +22,9 @@ def get_html(main_links):
         div_class = main_links.get(link)
         response = requests.get(link, headers = headers).text
         tree = html.fromstring(response)
-        news_items = tree.xpath("//div[@class='{0}']/*|//article[@class='{0}']/section/".format(div_class))
+        news_items = tree.xpath("//div[@class='{0}']/*".format(div_class))
         if link == 'https://korrespondent.net':
             source_name = 'Корреспондент'
-            print(len(news_items))
             for i in news_items:
                 news_data = {}
                 news_name = i.xpath(".//a/text()")
@@ -37,7 +38,6 @@ def get_html(main_links):
 
         elif link == 'https://www.rbc.ua':
             source_name = 'РБК'
-            print(len(news_items))
             for i in news_items:
                 news_data = {}
                 news_name = i.xpath(".//a/text()")
@@ -50,8 +50,8 @@ def get_html(main_links):
                 news_list.append(news_data)
 
         elif link == 'https://www.ukr.net/ru':
+            news_items = tree.xpath("//div[@class='feed__item--title']".format(div_class))
             source_name = 'УКР.NET'
-            print(len(news_items))
             for i in news_items:
                 news_data = {}
                 news_name = i.xpath(".//a/text()")
@@ -62,8 +62,11 @@ def get_html(main_links):
                 news_data['time'] = news_time
                 news_data['link'] = news_link
                 news_list.append(news_data)
-    print(news_list)
+
 
 
 get_html(main_links)
-
+pprint(news_list)
+client = MongoClient('localhost', 27017)
+db = client['news_database']
+db.collection.insertMany(news_list)
