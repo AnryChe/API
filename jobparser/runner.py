7008338fd05111b8e8b425b72be418c0980a6e22
AnryChe <stroyspecialist@rambler.ru>
2020-03-25 13:09:55 +0200

    Scrapy superjob spider

---
+++ jobparser/runner.py
@@ -0,0 +1,14 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+from jobparser import settings
+from jobparser.spiders.hhru import HhruSpider
+from jobparser.spiders.sjru import SjruSpider
+
+
+if __name__ =='__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+    process = CrawlerProcess(settings=crawler_settings)
+    process.crawl(HhruSpider)
+    process.crawl(SjruSpider)
+    process.start()
