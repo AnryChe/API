e87a13812a1aaae42d48f826e61e34007cd238e3
AnryChe <stroyspecialist@rambler.ru>
2020-04-02 14:39:22 +0300

    next step, get message data

--- selenium/main.py
+++ selenium/main.py
@@ -4,7 +4,7 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
+from pymongo import MongoClient
 
 
 driver = webdriver.Firefox()
@@ -39,23 +39,47 @@ def spam_list(driver):
         pass
 
 def get_message(links):
+
     for i in links:
         link = i.get_attribute('href')
-        print(link)
         driver.get(link)
-        from_who = driver.find_elements_by_class_name('readmsg__head-contact main').get_attribute('data-email')
-        shipping_data = driver.find_elements_by_class_name('readmsg__head-date')
-        theme_letter = driver.find_elements_by_class_name('readmsg__subject')
-        body_letter = driver.find_elements_by_class_name('readmsg__body')
+
+# ДВА ВАРИАНТА, С ВАЙТОМ И БЕЗ. Вайт вываливается в эксепт, хотя элемент на страничке есть  (xpath находит по  //*[@class='readmsg__head-contact main']
+# Вариант 1, с wait:
+        try:
+            from_who = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'readmsg__head-contact main'))
+            )
+        except:
+            print('элемент не обнаружен')
+        finally:
+            pass
+        from_who = from_who.get_attribute('data-email')
+# Вариант 2. Без wait
+
+        from_who = driver.find_element_by_class_name('readmsg__head-contact').get_attribute('data-email')
+        print(from_who)
 
 
 
+        shipping_data = driver.find_element_by_class_name('readmsg__head-date')
+        theme_letter = driver.find_element_by_class_name('readmsg__subject')
+        body_letter = driver.find_element_by_class_name('readmsg__body')
+        got_letter = {'From':from_who,
+                       'Data':shipping_data,
+                       'theme':theme_letter,
+                       'Letter':body_letter}
+        # un_base.insert_one(got_letter)
 
 
 my_login = 'kjdfshgmnjkew'
 my_password = '22Renegatspekulant22'
+client = MongoClient('localhost', 27017)
+un_base = client.ukr_net
 
 get_message(spam_list(get_messages_ukrnet(my_login, my_password)))
+
+# //*[@class='readmsg__head-contact main']
     # yield pass
 #
 # //a[@class='msglist__row']
